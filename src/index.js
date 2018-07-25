b922ef1c14ddd4dcdf1a655421522ffd9cd9d4a4
Jude Nelson <jude@blockstack.com>
2018-07-25 16:05:32 -0400

    fix isomorphic-fetch from blockstack.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,13 @@ const blockstack = require('blockstack')
 // workaround bug in blockstack.js
 global['window'] = { location: { origin: false } }
 
+// override blockstack's node-fetch with cross-fetch
+// (since we need response.body.arrayBuffer() support)
+global['fetch'] = require('cross-fetch')
+global['Headers'] = global['fetch'].Headers
+global['Request'] = global['fetch'].Request
+global['Response'] = global['fetch'].Response
+
 const conf = getConfig()
 const app = makeHttpServer(conf)
 
